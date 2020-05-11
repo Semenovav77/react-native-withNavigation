d34b4682de4ce6502307234f6bf75188bed835d3
semenovav <semenovav77@gmail.com>
2020-05-11 10:57:07 +0300

    TasksList with add-btn

--- App.js
+++ App.js
@@ -1,19 +1,194 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef} from 'react';
+import {Text, SectionList} from 'react-native';
+import styled from 'styled-components/native';
+import {MaterialIcons} from '@expo/vector-icons';
+
+import {Tasks} from "./src/components";
+
+const DATA = [
+        {
+            title: "09 мая 2020",
+            data: [
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '08:30',
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '07:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '06:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '06:00'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '05:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '07:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '06:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '06:00'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '05:30'
+                }
+            ]
+        },
+        {
+            title: "11 мая 2020",
+            data: [
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '08:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '07:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '07:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '06:30'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '06:00'
+                },
+                {
+                    user: {
+                        fullname: 'Вася Пупкин',
+                        avatar: 'https://sun9-58.userapi.com/c846218/v846218416/efa93/PufoAu6mPwU.jpg?ava=1',
+                    },
+                    task: 'Заключить договор1',
+                    time: '05:30'
+                }
+            ]
+        }
+    ]
+;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const scrll = useRef(null);
+    return (
+        <Container>
+            <Item>
+                <SectionList
+                    sections={DATA}
+                    keyExtractor={(item, index) => index}
+                    renderItem={({item}) => <Tasks {...item}/>}
+                    renderSectionHeader={({section: {title}}) => (
+                        <ItemTitle>
+                            <Text>{title}</Text>
+                        </ItemTitle>
+                    )}
+                />
+                <AddButton activeOpacity={0.5}>
+                    <MaterialIcons name="add" size={34} color="white" />
+                </AddButton>
+            </Item>
+        </Container>
+
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+    flex:1;
+    `;
+
+
+const Item = styled.View`
+    margin-top: 50px;
+`;
+
+const ItemTitle = styled.Text`
+    margin-bottom: 15px;
+    font-size: 25px;
+    background-color: white;
+    font-weight: 700;
+    padding: 0 20px;
+`;
+
+const AddButton = styled.TouchableOpacity`
+    position:absolute;
+    right: 30px;
+    bottom: 30px;
+    background-color: #28c75d;
+    width: 60px;
+    height: 60px;
+    border-radius: 50px
+    justify-content: center;
+    align-items: center
+`;
+
---
+++ src/components/ItemInfo.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from "styled-components/native";
+
+const Tasks = ({user, task, time}) => {
+    return (
+        <ItemInfo>
+            <Avatar
+                source={{
+                    uri: user.avatar,
+                }}/>
+            <ItemInfoBlock>
+                <FullName>{user.fullname}</FullName>
+                <Task>{task}</Task>
+            </ItemInfoBlock>
+            <BlockTime >
+                <Time >{time}</Time>
+            </BlockTime>
+        </ItemInfo>
+    );
+};
+
+export default Tasks;
+
+const Avatar = styled.Image`
+    width: 40px;
+    height: 40px;
+    border-radius: 40px;
+    margin-right: 16px;
+`;
+const ItemInfoBlock = styled.View`
+    flex: 1;  
+`;
+const FullName = styled.Text`
+    font-size: 18px
+`;
+const Task = styled.Text`
+    opacity: 0.4
+    font-size: 14px
+`;
+const Time = styled.Text`
+    font-size: 18px;
+    color: ${props => props.primary ? 'white' : '#45b0d1'} 
+`;
+const BlockTime = styled.View`
+    background: ${props => props.primary ? '#005bd1' : '#e9f5ff'};
+    font-weight: 600
+    padding: 5px 15px;
+    border-radius: 15px
+`;
+const ItemInfo = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 10px;
+    border-bottom-width: 1px 
+    border-bottom-color: #f3f3f3;
+    padding: 20px 20px
+`;
